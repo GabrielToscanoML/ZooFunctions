9b70d8581c0ca407ecbf3cc292c058402de94f38
Gabriel Toscano <gabrieltoscano1@outlook.com>
2022-08-26 14:36:03 -0300

    iniciando o projeto

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,29 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(ids) {
   // seu código aqui
+  const arraySpecies = [];
+  if (ids === '') {
+    return arraySpecies;
+  }
+
+  species.forEach((element) => {
+    if (species.find(ids)) {
+      arraySpecies.push(element);
+    }
+    return arraySpecies;
+  });
 }
 
 module.exports = getSpeciesByIds;
+  //  else {
+  //   species.forEach((element) => {
+  //     ids.forEach((elemento) => {
+  //       if (element.id === elemento) {
+  //         arraySpecies.push(element);
+  //       }
+  //     });
+  //   });
+  //   return arraySpecies;
+  // }
