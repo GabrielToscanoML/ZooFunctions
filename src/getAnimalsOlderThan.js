84aa7caee88a2885d1b96aab327adcdd5644de98
Gabriel Toscano <gabrieltoscano1@outlook.com>
2022-08-26 16:48:57 -0300

    requisito 1 completo

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,15 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  data.forEach((element) => {
+    element.includes((element.name === animal) => {
+      if (element.age >= age) {
+        return true;
+      }
+      return false;
+      // animal.age >= age ? true : false;
+    });
+  });
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,29 +1,17 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
-  const arraySpecies = [];
-  if (ids === '') {
-    return arraySpecies;
-  }
-
-  species.forEach((element) => {
-    if (species.find(ids)) {
-      arraySpecies.push(element);
-    }
-    return arraySpecies;
+  const resultado = [];
+  species.filter((element) => {
+    ids.forEach((elemento) => {
+      if (element.id === elemento) {
+        resultado.push(element);
+      }
+    });
+    return resultado;
   });
+  return resultado;
 }
-
 module.exports = getSpeciesByIds;
-  //  else {
-  //   species.forEach((element) => {
-  //     ids.forEach((elemento) => {
-  //       if (element.id === elemento) {
-  //         arraySpecies.push(element);
-  //       }
-  //     });
-  //   });
-  //   return arraySpecies;
-  // }
