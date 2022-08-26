0f34a7690c3459e28c2e392ef82e15a2bfc8d716
Gabriel Toscano <gabrieltoscano1@outlook.com>
2022-08-26 17:45:40 -0300

    requisito 3 concluido

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,21 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  let result = {
+
+  };
+
+  data.employees.filter((funcionario) => {
+    if (funcionario.firstName === employeeName) {
+      result = funcionario;
+    }
+    if (funcionario.lastName === employeeName) {
+      result = funcionario;
+    }
+    return result;
+  });
+
+  return result;
 }
 
 module.exports = getEmployeeByName;
