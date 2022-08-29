d34340e1fee78997eafa048128129e3e05a42a1d
Gabriel Toscano <gabrieltoscano1@outlook.com>
2022-08-29 15:44:25 -0300

    Requisitos 4 e 5 finalizados

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,51 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function getResidentsLenght(animal) {
+  const { specie } = animal;
+  let result;
+  data.species.forEach((element) => {
+    if (element.name === specie) {
+      result = element.residents.length;
+    }
+  });
+  return result;
 }
 
+function getResidentsSexLength(animal) {
+  const { specie } = animal;
+  const { sex } = animal;
+  let count = 0;
+  let result;
+  data.species.forEach((element) => {
+    if (element.name === specie) {
+      element.residents.forEach((elemento) => {
+        if (elemento.sex === sex) {
+          count += 1;
+        }
+      });
+    }
+    result = count;
+  });
+  return result;
+}
+
+function countAnimals(animal) {
+  // seu código aqui
+  if (typeof animal !== 'undefined') {
+    const objKeysLength = Object.keys(animal).length;
+    if (objKeysLength === 1) {
+      return getResidentsLenght(animal);
+    }
+    if (objKeysLength === 2) {
+      return getResidentsSexLength(animal);
+    }
+  }
+  const obj = {};
+  const add = (element) => {
+    obj[`${element.name}`] = element.residents.length;
+  };
+  data.species.forEach(add);
+  console.log(obj);
+  return obj;
+}
 module.exports = countAnimals;
