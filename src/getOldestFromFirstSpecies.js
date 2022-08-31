9aa30d1ccf11fa48fb2698279189f7d4ce8db603
Gabriel Toscano <gabrieltoscano1@outlook.com>
2022-08-31 13:48:24 -0300

    requisito 10 concluido

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
+function returnEmployee(id) {
+  return data.employees.find((element) => element.id === id);
+}
+
+function returnAnimal(animalID) {
+  return data.species.find((element) => element.id === animalID);
+}
+
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  // pega o primeiro funcionário e a primeira espécie
+  const employee = returnEmployee(id).responsibleFor[0];
+  // pega os animais da e espécie
+  const animais = returnAnimal(employee).residents;
+
+  const ordenaAnimais = animais.sort((animalA, animalB) => animalB.age - animalA.age)[0];
+
+  const resultado = [`${ordenaAnimais.name}`, `${ordenaAnimais.sex}`, ordenaAnimais.age];
+
+  return resultado;
 }
 
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,72 @@
 const data = require('../data/zoo_data');
+const { hours } = data;
+
+function completeObject(obj) {
+  const newOBj = obj;
+  const arrayKeys = Object.keys(hours);
+  arrayKeys.forEach((element) => {
+    let day = `${hours.element}`;
+    const day = {
+
+    },
+  });
+  // console.log(arrayKeys);
+}
+
+
+function createObject() {
+  const obj = {
+    Tuesday: {},
+    Wednesday: {},
+    Thursday: {},
+    Friday: {},
+    Saturday: {},
+    Sunday: {},
+    Monday: {},
+  };
+  completeObject(obj);
+  return obj;
+}
 
 function getSchedule(scheduleTarget) {
   // seu código aqui
+
+  if (!scheduleTarget) {
+    return createObject();
+  }
+
+  const animal = data.species.find((element) => (element.name === scheduleTarget));
+  return animal.availability;
 }
 
 module.exports = getSchedule;
+console.log(getSchedule());
+
+// Tuesday: {
+//   officeHour: `Open from ${hours.Tuesday.open} until ${hours.Tuesday.close}`,
+//   exibithion: getAnimals(Tuesday),
+// },
+// Wednesday: {
+//   officeHour: `Open from ${hours.Wednesday.open} until ${hours.Wednesday.close}`,
+//   exibithion: getAnimals(Wednesday),
+// },
+// Thursday: {
+//   officeHour: `Open from ${hours.Thursday.open} until ${hours.Thursday.close}`,
+//   exibithion: getAnimals(Thursday),
+// },
+// Friday:{
+//   officeHour: `Open from ${hours.Friday.open} until ${hours.Friday.close}`,
+//   exibithion: getAnimals(Friday),
+// },
+// Saturday: {
+//   officeHour: `Open from ${hours.Saturday.open} until ${hours.Saturday.close}`,
+//   exibithion: getAnimals(Saturday),
+// },
+// Sunday: {
+//   officeHour: `Open from ${hours.Sunday.open} until ${hours.Sunday.close}`,
+//   exibithion: getAnimals(Sunday),
+// },
+// Monday: {
+//   officeHour: `Open from ${hours.Monday.open} until ${hours.Monday.close}`,
+//   exibithion: getAnimals(Monday),
+// },
